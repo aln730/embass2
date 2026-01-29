6f05dcf36aeabee8667966a40335176de9cd8c55
AGawas <arnavgawas707@gmail.com>
2026-01-29 14:41:17 -0500

    Refactor and update assembly program structure

--- ex2.s
+++ ex2.s
@@ -1,195 +1,249 @@
-TTL Program Title: Lab 3 - Arithmetic Computation with Overflow
+            TTL Program Title for Listing Header Goes Here
 ;****************************************************************
-;Lab 3: Memory, Conditional Branching, and Debugging Tools
-;This program computes:
-; F = 3P + 2Q - 75
-; G = 2P - 4Q + 63
-; Result = F + G
-; The program checks for byte overflow for each computation.
-; Name:  Arnav Gawas
-; Date:  01/29/2026
-; Class: CMPE-250
-; Section:  TBD
+;Descriptive comment header goes here.
+;(What does the program do?)
+;Name:  <Your name here>
+;Date:  <Date completed here>
+;Class:  CMPE-250
+;Section:  <Your lab section, day, and time here>
 ;---------------------------------------------------------------
 ;Keil Simulator Template for KL05
 ;R. W. Melton
 ;August 21, 2025
 ;****************************************************************
-
+;Assembler directives
             THUMB
             OPT    64  ;Turn on listing macro expansions
-
 ;****************************************************************
-;EQUates for multiplication shifts
-MUL1    EQU 1
-MUL2    EQU 1  ; multiply by 2 = shift left 1
-MUL4    EQU 2  ; multiply by 4 = shift left 2
-
-;****************************************************************
-;Constants
-            AREA    MyConst,DATA,READONLY
-const_F     DCD 75
-const_G     DCD 63
-;>>>>> begin constants here <<<<<
-;>>>>>   end constants here <<<<<
-            ALIGN
-;****************************************************************
-;Variables
-            AREA    MyData,DATA,READWRITE
-P           DCD 0
-Q           DCD 0
-F           DCD 0
-G           DCD 0
-Result      DCD 0
-;>>>>> begin variables here <<<<<
-;>>>>>   end variables here <<<<<
-            ALIGN
+;EQUates
+;Standard data masks
+BYTE_MASK         EQU  0xFF
+NIBBLE_MASK       EQU  0x0F
+;Standard data sizes (in bits)
+BYTE_BITS         EQU  8
+NIBBLE_BITS       EQU  4
+;Architecture data sizes (in bytes)
+WORD_SIZE         EQU  4  ;Cortex-M0+
+HALFWORD_SIZE     EQU  2  ;Cortex-M0+
+;Architecture data masks
+HALFWORD_MASK     EQU  0xFFFF
+;Return                 
+RET_ADDR_T_MASK   EQU  1  ;Bit 0 of ret. addr. must be
+                          ;set for BX, BLX, or POP
+                          ;mask in thumb mode
+;---------------------------------------------------------------
+;Vectors
+VECTOR_TABLE_SIZE EQU 0x000000C0  ;KL05
+VECTOR_SIZE       EQU 4           ;Bytes per vector
+;---------------------------------------------------------------
+;CPU CONTROL:  Control register
+;31-2:(reserved)
+;   1:SPSEL=current stack pointer select
+;           0=MSP (main stack pointer) (reset value)
+;           1=PSP (process stack pointer)
+;   0:nPRIV=not privileged
+;        0=privileged (Freescale/NXP "supervisor") (reset value)
+;        1=not privileged (Freescale/NXP "user")
+CONTROL_SPSEL_MASK   EQU  2
+CONTROL_SPSEL_SHIFT  EQU  1
+CONTROL_nPRIV_MASK   EQU  1
+CONTROL_nPRIV_SHIFT  EQU  0
+;---------------------------------------------------------------
+;CPU PRIMASK:  Interrupt mask register
+;31-1:(reserved)
+;   0:PM=prioritizable interrupt mask:
+;        0=all interrupts unmasked (reset value)
+;          (value after CPSIE I instruction)
+;        1=prioritizable interrrupts masked
+;          (value after CPSID I instruction)
+PRIMASK_PM_MASK   EQU  1
+PRIMASK_PM_SHIFT  EQU  0
+;---------------------------------------------------------------
+;CPU PSR:  Program status register
+;Combined APSR, EPSR, and IPSR
+;----------------------------------------------------------
+;CPU APSR:  Application Program Status Register
+;31  :N=negative flag
+;30  :Z=zero flag
+;29  :C=carry flag
+;28  :V=overflow flag
+;27-0:(reserved)
+APSR_MASK     EQU  0xF0000000
+APSR_SHIFT    EQU  28
+APSR_N_MASK   EQU  0x80000000
+APSR_N_SHIFT  EQU  31
+APSR_Z_MASK   EQU  0x40000000
+APSR_Z_SHIFT  EQU  30
+APSR_C_MASK   EQU  0x20000000
+APSR_C_SHIFT  EQU  29
+APSR_V_MASK   EQU  0x10000000
+APSR_V_SHIFT  EQU  28
+;----------------------------------------------------------
+;CPU EPSR
+;31-25:(reserved)
+;   24:T=Thumb state bit
+;23- 0:(reserved)
+EPSR_MASK     EQU  0x01000000
+EPSR_SHIFT    EQU  24
+EPSR_T_MASK   EQU  0x01000000
+EPSR_T_SHIFT  EQU  24
+;----------------------------------------------------------
+;CPU IPSR
+;31-6:(reserved)
+; 5-0:Exception number=number of current exception
+;      0=thread mode
+;      1:(reserved)
+;      2=NMI
+;      3=hard fault
+;      4-10:(reserved)
+;     11=SVCall
+;     12-13:(reserved)
+;     14=PendSV
+;     15=SysTick
+;     16=IRQ0
+;     16-47:IRQ(Exception number - 16)
+;     47=IRQ31
+;     48-63:(reserved)
+IPSR_MASK             EQU  0x0000003F
+IPSR_SHIFT            EQU  0
+IPSR_EXCEPTION_MASK   EQU  0x0000003F
+IPSR_EXCEPTION_SHIFT  EQU  0
+;----------------------------------------------------------
+PSR_N_MASK           EQU  APSR_N_MASK
+PSR_N_SHIFT          EQU  APSR_N_SHIFT
+PSR_Z_MASK           EQU  APSR_Z_MASK
+PSR_Z_SHIFT          EQU  APSR_Z_SHIFT
+PSR_C_MASK           EQU  APSR_C_MASK
+PSR_C_SHIFT          EQU  APSR_C_SHIFT
+PSR_V_MASK           EQU  APSR_V_MASK
+PSR_V_SHIFT          EQU  APSR_V_SHIFT
+PSR_T_MASK           EQU  EPSR_T_MASK
+PSR_T_SHIFT          EQU  EPSR_T_SHIFT
+PSR_EXCEPTION_MASK   EQU  IPSR_EXCEPTION_MASK
+PSR_EXCEPTION_SHIFT  EQU  IPSR_EXCEPTION_SHIFT
+;----------------------------------------------------------
+;Stack
+SSTACK_SIZE EQU  0x00000100
 ;****************************************************************
 ;Program
 ;Linker requires Reset_Handler
             AREA    MyCode,CODE,READONLY
             ENTRY
             EXPORT  Reset_Handler
-
 Reset_Handler  PROC {}
-            ; Initialize registers
+main
+;---------------------------------------------------------------
+;Initialize registers
             BL      RegInit
-
 ;>>>>> begin main program code <<<<<
-            BL      ComputeF
-            BL      ComputeG
-            BL      ComputeResult
+            AREA    MyCode,CODE,READONLY
+            ENTRY
+            EXPORT  Reset_Handler
+
+Reset_Handler PROC
 ;>>>>>   end main program code <<<<<
-
-;Stay here
-            B       .
-            ENDP    ;Reset_Handler
-;---------------------------------------------------------------
-RegInit     PROC {}
-;********************************************************************
-;Initializes registers R1 = P, R2 = Q from memory
-;********************************************************************
             LDR     R1,=P
-            LDR     R1,[R1]
+            LDR     R1,[R1]       ; R1 = P
             LDR     R2,=Q
-            LDR     R2,[R2]
-            BX      LR
-            ENDP    ;RegInit
+            LDR     R2,[R2]       ; R2 = Q
+
 ;---------------------------------------------------------------
-ComputeF    PROC {}
-;********************************************************************
-;Computes F = 3P + 2Q - 75
-;Checks for byte overflow [-128, 127] after each operation
-;********************************************************************
-            PUSH    {R4-R5,LR}
+; Compute F = 3P + 2Q - 75
+; 3P = (P<<1)+P
+            MOV     R3,R1
+            ADD     R3,R3,R3      ; 2P
+            ADD     R3,R3,R1      ; 3P
 
-            ; 3P = 2P + P
-            MOV     R4,R1
-            LSL     R5,R1,MUL1
-            ADDS    R4,R4,R5
-            CMP     R4,#127
-            BGT     F_Overflow
-            CMP     R4,#-128
-            BLT     F_Overflow
+; 2Q = Q<<1
+            MOV     R4,R2
+            ADD     R4,R4,R4      ; 2Q
 
-            ; 3P + 2Q
-            LSL     R5,R2,MUL1
-            ADDS    R4,R4,R5
-            CMP     R4,#127
-            BGT     F_Overflow
-            CMP     R4,#-128
-            BLT     F_Overflow
+; F = 3P + 2Q
+            ADD     R5,R3,R4
 
-            ; Subtract const_F
-            LDR     R5,=const_F
-            LDR     R5,[R5]
-            SUBS    R4,R4,R5
-            CMP     R4,#127
-            BGT     F_Overflow
-            CMP     R4,#-128
-            BLT     F_Overflow
+; F = F - 75
+            LDR     R6,=const_F
+            LDR     R6,[R6]
+            SUB     R5,R5,R6
 
-            ; Store F
-            LDR     R5,=F
-            STR     R4,[R5]
-            B       F_End
+; Store F
+            LDR     R7,=F
+            STR     R5,[R7]
 
-F_Overflow:
-            LDR     R5,=F
-            MOV     R4,#0
-            STR     R4,[R5]
-
-F_End:
-            POP     {R4-R5,LR}
-            BX      LR
-            ENDP    ;ComputeF
 ;---------------------------------------------------------------
-ComputeG    PROC {}
-;********************************************************************
-;Computes G = 2P - 4Q + 63
-;Uses V flag to detect overflow
-;********************************************************************
-            PUSH    {R4-R6,LR}
+; Compute G = 2P - 4Q + 63
+; 2P = P<<1
+            MOV     R3,R1
+            ADD     R3,R3,R3      ; 2P
 
-            ; 2P
-            LSL     R4,R1,MUL1
+; 4Q = Q<<2
+            MOV     R4,R2
+            ADD     R4,R4,R4      ; 2Q
+            ADD     R4,R4,R4      ; 4Q
 
-            ; -4Q
-            LSL     R5,R2,MUL2
-            SUBS    R4,R4,R5
-            BVS     G_Overflow
+; 2P - 4Q
+            RSBS    R4,R4,#0      ; Negate 4Q
+            ADD     R5,R3,R4      ; 2P - 4Q
 
-            ; Add const_G
-            LDR     R5,=const_G
-            LDR     R5,[R5]
-            ADDS    R4,R4,R5
-            BVS     G_Overflow
+; +63
+            LDR     R6,=const_G
+            LDR     R6,[R6]
+            ADD     R5,R5,R6      ; G = 2P - 4Q + 63
 
-            ; Store G
-            LDR     R6,=G
-            STR     R4,[R6]
-            B       G_End
+; Store G
+            LDR     R7,=G
+            STR     R5,[R7]
 
-G_Overflow:
-            LDR     R6,=G
-            MOV     R4,#0
-            STR     R4,[R6]
-
-G_End:
-            POP     {R4-R6,LR}
-            BX      LR
-            ENDP    ;ComputeG
 ;---------------------------------------------------------------
-ComputeResult PROC {}
-;********************************************************************
-;Computes Result = F + G
-;Checks for byte overflow [-128, 127]
-;********************************************************************
-            PUSH    {R4-R5,LR}
-
-            LDR     R4,=F
+; Compute Result = F + G
+            LDR     R3,=F
+            LDR     R3,[R3]
+            LDR     R4,=G
             LDR     R4,[R4]
-            LDR     R5,=G
-            LDR     R5,[R5]
-            ADDS    R4,R4,R5
-            CMP     R4,#127
-            BGT     R_Overflow
-            CMP     R4,#-128
-            BLT     R_Overflow
+            ADD     R5,R3,R4
+            LDR     R7,=Result
+            STR     R5,[R7]
 
-            LDR     R5,=Result
-            STR     R4,[R5]
-            B       R_End
+;---------------------------------------------------------------
+; End program: loop here
+            B       .
 
-R_Overflow:
-            LDR     R5,=Result
-            MOV     R4,#0
-            STR     R4,[R5]
-
-R_End:
-            POP     {R4-R5,LR}
-            BX      LR
-            ENDP    ;ComputeResult
+            ENDP
+;---------------------------------------------------------------
+RegInit     PROC  {}
+;********************************************************************
+;Initializes register n to value 0xnnnnnnnn, for n in {0x1-0xC,0xE}.
+;Initializes R0 to 0x05250821.
+;Initializes APSR.NZCV to 2_1111.
+;********************************************************************
+;Put return on stack
+            PUSH    {LR}
+;Initialize registers
+            LDR     R1,=0x11111111
+            ADDS    R2,R1,R1
+            ADDS    R3,R2,R1
+            ADDS    R4,R3,R1
+            ADDS    R5,R4,R1
+            ADDS    R6,R5,R1
+            ADDS    R7,R6,R1
+            MOV     R8,R1
+            ADD     R8,R8,R7
+            MOV     R9,R1
+            ADD     R9,R9,R8
+            MOV     R10,R1
+            ADD     R10,R10,R9
+            MOV     R11,R1
+            ADD     R11,R11,R10
+            MOV     R12,R1
+            ADD     R12,R12,R11
+            MOV     R14,R2
+            ADD     R14,R14,R12
+            MOV     R0,R1
+            ADD     R0,R0,R14
+            MSR     APSR,R0
+            LDR     R0,=0x05250821
+            POP     {PC}
+            ENDP    ;RegInit
 ;---------------------------------------------------------------
 ;>>>>> begin subroutine code <<<<<
 ;>>>>>   end subroutine code <<<<<
@@ -213,7 +267,12 @@ __Vectors_Size  EQU     __Vectors_End - __Vectors
 ;Constants
             AREA    MyConst,DATA,READONLY
 ;>>>>> begin constants here <<<<<
+const_F      DCD 75
+const_G      DCD 63
 ;>>>>>   end constants here <<<<<
+MUL2    EQU 1
+MUL3    EQU 1
+MUL4    EQU 2
 ;****************************************************************
             AREA    |.ARM.__at_0x1FFFFC00|,DATA,READWRITE,ALIGN=3
             EXPORT  __initial_sp
@@ -227,5 +286,10 @@ __initial_sp
 ;Variables
             AREA    MyData,DATA,READWRITE
 ;>>>>> begin variables here <<<<<
+P           DCD 0
+Q           DCD 0
+F           DCD 0
+G           DCD 0
+Result      DCD 0
 ;>>>>>   end variables here <<<<<
             END
